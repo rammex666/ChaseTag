69f836c250270597a5a12f521bed9e0ef4e5feb1
.rammex <104463176+rammex666@users.noreply.github.com>
2026-04-10 15:38:48 +0200

    Update PlayerLobbyEvents.java

--- lobby/src/main/java/fr/rammex/chasetag/lobby/player/events/PlayerLobbyEvents.java
+++ lobby/src/main/java/fr/rammex/chasetag/lobby/player/events/PlayerLobbyEvents.java
@@ -9,10 +9,14 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerLobbyEvents implements Listener {
 
